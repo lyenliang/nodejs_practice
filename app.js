77db114286360c3b458aafecc4115e1376094c58
lyenliang <i745m@hotmail.com>
2016-12-12 17:01:43 +0800

    Avoid duplicated code

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var app = express();
 var logger = require('./logger');
 // loogger is used to record the duration of every action, so it must be the first middleware
 app.use(logger);
-
 app.use(express.static('public'));
 
 var bodyParser = require('body-parser');
@@ -20,21 +19,44 @@ var locations = {
     'Fixed': 'First floor', 'Movable': 'Second floor', 'Rotating': 'Penthouse'
 };
 
-app.delete('/blocks/:name', function(request, response) {
-    delete blocks[request.blockName];
-    
-    // sendStatus sets the response body to OK
-    response.sendStatus(200);
-});
+// dynamic route
+app.route('/blocks/:name')
+    .get(function(request, response) {
+        // curl -i http://localhost:3000/blocks/Fixed
+        var description = blocks[request.blockName];
+        if (!description) {
+            // description is undefined
+            response.status(404).json('No description found for ' + request.params.name);
+        } else {
+            response.json(description);
+        }
+    })
+    .delete(function(request, response) {
+        delete blocks[request.blockName];
+        
+        // sendStatus sets the response body to OK
+        response.sendStatus(200);
+    });
 
-app.post('/blocks', parseUrlencoded, function(request, response) {
-    var newBlock = request.body;
-    blocks[newBlock.name] = newBlock.description;
+app.route('/blocks')
+    .get(function(request, response) {
+        if (request.query.limit >= 0) {
+            // localhost:3000/blocks?limit=0
+            // localhost:3000/blocks?limit=1
+            // ...
+            response.json(Object.keys(blocks).slice(0, request.query.limit));
+        } else {
+            response.json(Object.keys(blocks));
+        }
+    })
+    .post(parseUrlencoded, function(request, response) {
+        var newBlock = request.body;
+        blocks[newBlock.name] = newBlock.description;
+        
+        // 201 stands for "created"
+        response.status(201).json(newBlock.name);
+    });
     
-    // 201 stands for "created"
-    response.status(201).json(newBlock.name);
-});
-
 // Accessing custom properties on request
 app.param('name', function(request, response, next) {
     var name = request.params.name;
@@ -44,17 +66,7 @@ app.param('name', function(request, response, next) {
     next();
 });
 
-// dynamic route
-app.get('/blocks/:name', function(request, response) {
-    // curl -i http://localhost:3000/blocks/Fixed
-    var description = blocks[request.blockName];
-    if (!description) {
-        // description is undefined
-        response.status(404).json('No description found for ' + request.params.name);
-    } else {
-        response.json(description);
-    }
-}); 
+
 
 app.get('/locations/:name', function(request, response) {
     // curl -i http://localhost:3000/blocks/Fixed
@@ -66,18 +78,6 @@ app.get('/locations/:name', function(request, response) {
         response.json(description);
     }
 }); 
-
-app.get('/blocks', function(request, response) {
-    if (request.query.limit >= 0) {
-        // localhost:3000/blocks?limit=0
-        // localhost:3000/blocks?limit=1
-        // ...
-        response.json(Object.keys(blocks).slice(0, request.query.limit));
-    } else {
-        response.json(Object.keys(blocks));
-    }
-});
-
 
 app.listen(3000, function() {
     console.log('Listening on port 3000');
